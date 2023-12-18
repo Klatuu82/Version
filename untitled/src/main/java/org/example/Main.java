656cd9290c8e6035a0eb03e326c64a93221aacc8
Marcel Daus <dausmarcel@hotmail.com>
2023-12-18 22:54:53 +0100

    Version 1.1.0

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hallo Verions 1.0.0");
+        System.out.println("Hallo Verions 1.1.0");
     }
 }
